8be6721a7c5b933dbf770134571eb1bd3c48be73
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2017-10-18 22:12:57 +0300

    Create a new game using LibGDX.

---
+++ android/src/com/mobile/pixelbird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mobile.pixelbird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mobile.pixelbird.PixalBirdGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PixalBirdGame(), config);
+	}
+}
